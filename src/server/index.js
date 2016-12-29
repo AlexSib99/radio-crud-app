3775375fe4e33de7360094fe06d8db9583561d13
AlexSib99 <luckyanov@gmail.com>
2016-12-30 04:15:45 +0700

    fix static server

--- src/server/index.js
+++ src/server/index.js
@@ -2,15 +2,16 @@ var express = require('express');
 
 var app = express();
 
+var path = require('path');
+
+app.use('/static', express.static( path.join(__dirname, '..', 'client')) );
+
+app.get('/list', (req, res) => {
+   res.send('List of radio releases');
+});
+
 app.listen(3000, (err) => {
    if (!err) {
       console.log('Server started on port 3000');
    }
-});
-
-var path = require('path');
-app.use('/static', path.join(__dirname, '..', 'client'));
-
-app.get('/list', (req, res) => {
-   res.send('List of radio releases');
 });
