cee59446345c69ce5316a89246e231757afb749a
AlexSib99 <luckyanov@gmail.com>
2017-01-29 02:06:03 +0700

    add table to /list page

--- src/client/list.js
+++ src/client/list.js
@@ -10,4 +10,19 @@ app.controller('listPageCtrl', ['$scope', function ($scope) {
 
 app.controller('secondCtrl', ['$scope', function ($scope) {
     $scope.numbers = [3, 5, 6, 8, 0];
+
+    $scope.headers = ['name', 'address', 'phone'];
+
+    $scope.data = [
+       {
+           name: 'John',
+           address: 'some street',
+           phone: '111-222-333'
+        },
+        {
+            name: 'Alex',
+            address: 'another street',
+            phone: '999-999-999'
+        }
+    ];
 }]);
